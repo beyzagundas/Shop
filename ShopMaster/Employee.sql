24afa4007a6ce7372e354ff1f97991f31fda48a0
beyzagundas <158465014+beyzagundas@users.noreply.github.com>
2025-07-04 09:36:10 +0300

    Update Employee.sql

--- ShopMaster/Employee.sql
+++ ShopMaster/Employee.sql
@@ -1,5 +1,5 @@
 insert into Employee(id, name, position, manager_id) values(71, 'Sevil Çetin', 'hr', null)
 insert into Employee(id, name, position, manager_id) values(72, 'Caner Koray', 'sr', null)
-insert into Employee(id, name, position, manager_id) values(73, 'Zeynep Günal', 'managemer', null)
+insert into Employee(id, name, position, manager_id) values(73, 'Zeynep Günal', 'manager', null)
 
 update Employee set manager_id=73 where id=72
